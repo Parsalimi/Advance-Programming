ae3f376725cc270c3ea7584d69ed9b176e4869fb
Parsa Salimi <salimi1382@gmail.com>
2024-04-10 13:31:21 +0330

    Fix Advance calculator

--- S03_14021214/test.py
+++ S03_14021214/test.py
@@ -1,1 +1,20 @@
-print("( 3 + 2 ) x 7".split(" "))
+solid_entry = '(23+40)x23'
+myList = []
+looking_list = ['(',')','+','-','x','/']
+last_important_index = 0
+
+for index, char in enumerate(solid_entry):
+    if char in looking_list:
+        if index > 0:
+            myList.append(solid_entry[last_important_index+1:index])
+            myList.append(char)
+            last_important_index = index
+        else:
+            myList.append(char)
+
+myList.append(solid_entry[last_important_index+1:len(solid_entry)]) # append the last 
+
+for item in myList:
+    if item == '':
+        myList.remove(item)
+print(myList)
