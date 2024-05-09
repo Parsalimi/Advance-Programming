6b0659f80be8c25436c169c0df10ef64b7d07ba3
Parsa Salimi <salimi1382@gmail.com>
2024-05-09 10:25:42 +0330

    Reorganize

--- S08_14030210/Vending/tool.py
+++
@@ -1,7 +0,0 @@
-class Tool:
-    def TxtContentReader(text):
-        main_list = text.split("/")
-        for index, item in enumerate(main_list):
-            main_list[index] = item.split(",")
-
-        return main_list
